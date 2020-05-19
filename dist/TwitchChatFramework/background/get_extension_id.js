97a2edca13be2424eb8f791351a370254d84ab00
sfsepark <sfsepark@gmail.com>
2020-05-19 16:18:20 +0900

    extension 끼리 충돌나던 버그 해결

--- dist/TwitchChatFramework/background/get_extension_id.js
+++ dist/TwitchChatFramework/background/get_extension_id.js
@@ -1,17 +1,17 @@
-chrome.runtime.onMessage.addListener(function(request, sender, callback){
-    if(request.type == 'get_extension_id'){
-        chrome.management.getSelf(function(result){
-            let uuid_reg = /[0-9a-zA-Z\-]+/;
-            let reg_res =  uuid_reg.exec(result);
-            let extension_id = result.id;
-            if(reg_res != null){
-                extension_id = reg_res[0];
-            }
-            callback({data:true, extension_id : extension_id});
+chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+  if (request.type == 'get_extension_id') {
+    chrome.management.getSelf(function (result) {
+      let uuid_reg = /[0-9a-zA-Z\-]+/;
+      let extension_id = result.id;
+      let reg_res = uuid_reg.exec(extension_id);
+      if (reg_res != null) {
+        extension_id = reg_res[0];
+      }
+      callback({ data: true, extension_id: extension_id });
 
-            return true;
-        })
-    }
+      return true;
+    });
+  }
 
-    return true;
+  return true;
 });
